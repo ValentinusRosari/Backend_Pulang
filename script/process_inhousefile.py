bee2a208c89911b35a59892c93513e6f42ea0077
zildirayalfirli <zildirayalfirli@gmail.com>
2024-06-21 04:23:35 +0700

    add logic

--- script/process_inhousefile.py
+++ script/process_inhousefile.py
@@ -38,7 +38,12 @@ def process_file(sample_file, input_file):
         df = clean_csv(input_file, expected_columns)
         delete_columns = ['VIP', 'Lunch', 'Dinner', 'Other', 'Rate Code', 'K-Card', 'remarks', 'Member No', 'Member Type', 'Guest No']
         df.drop(columns=delete_columns, inplace=True)
-        df['Name'] = df['Last Name']
+        df['First Name'].replace('', pd.NA, inplace=True)
+        df['First Name'].replace('NULL', pd.NA, inplace=True)
+        df['Last Name'].replace('', pd.NA, inplace=True)
+        df['Last Name'].replace('NULL', pd.NA, inplace=True)
+        df['Name'] = df.apply(lambda row: row['Last Name'] if pd.isnull(row['First Name']) else row['First Name'] + ' ' + row['Last Name'], axis=1)
+        df['Name'] = df['Name'].str.strip() 
         df.drop(columns=['First Name', 'Last Name'], inplace=True)
         df['In House Date'] = pd.to_datetime(df['In House Date'], format='%d/%m/%Y').dt.strftime('%d %B %Y')
         df['Arrival'] = pd.to_datetime(df['Arrival'], format='%d/%m/%Y')
